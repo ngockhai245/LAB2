bf057efecd497f5f5fad7afe432e76959dfd72af
ngockhai245 <phamngockhai245@gmail.com>
2023-10-31 22:32:48 +0700

    Finish Ex6

--- STM32_LAB2/Core/Src/main.c
+++ STM32_LAB2/Core/Src/main.c
@@ -57,6 +57,19 @@ void updateClockBuffer();
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 int hour = 4, minute = 7, second = 3;
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;
+void setTimer0(int duration){
+	timer0_counter = duration /TIMER_CYCLE;
+	timer0_flag = 0;
+}
+void timer_run(){
+	if(timer0_counter > 0){
+		timer0_counter--;
+		if(timer0_counter == 0) timer0_flag = 1;
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -98,25 +111,12 @@ int main(void)
 
 
 
-  while (1)
-  {
-    /* USER CODE END WHILE */
-	  second++;
-	  if(second >= 60){
-		  second = 0;
-		  minute++;
-	  }
-	  if(minute >= 60){
-		  minute = 0;
-		  hour++;
-	  }
-	  if(hour >= 24){
-		  hour = 0;
-	  }
-	  updateClockBuffer();
-	  HAL_Delay(1000);
-
-    /* USER CODE BEGIN 3 */
+  setTimer0(1000);
+  while (1){
+      if(timer0_flag == 1){
+          HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+          setTimer0(2000);
+      }
   }
   /* USER CODE END 3 */
 }
@@ -406,6 +406,7 @@ int LED_status = 0;
 int dot_LED_counter = 100;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	timer_run();
 	counter--;
 	dot_LED_counter--;
 	if (dot_LED_counter <= 0){
